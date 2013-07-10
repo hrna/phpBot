4e0521c7a7bd9bd956578081c2c740841987b5b7
Marko Mäkilä <hrna@lakka.kapsi.fi>
2013-07-10 15:54:25 +0300

    test on the way

---
+++ modules/wiki_module.php
@@ -0,0 +1,23 @@
+<?php
+class wiki
+	{
+	public function wiki($haku)
+		{
+			$string_num = count($haku);
+			$result = array();
+			for ($i = 4; $i < $string_num; $i++)
+			{
+				if ($haku[$i] != "") 
+				{
+					$result[$i] .= $haku[$i]; #undefined offsetti, fixii tähä...
+				}
+			}				
+			$result = implode("_",$result);
+			$result = "http://en.wikipedia.org/wiki/".ucfirst($result); #Eka kirjain isoksi
+			return $result;
+		}
+
+
+	}
+
+?>
