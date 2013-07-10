61869d0cd519bbdf7a0ab2a62f2c2996e4aac175
Jaakko Kuusela <shimatta@gmail.com>
2013-07-10 20:28:11 +0300

    Wiki fixes

--- modules/wiki_module.php
+++ modules/wiki_module.php
@@ -1,6 +1,10 @@
 <?php
 function wiki($haku)
 		{
+		if ($haku[4] == "")
+		{
+			return "Usage: !wiki MS Silja Europa";
+		} else {
 			$string_num = count($haku);
 			$result = array();
 			for ($i = 4; $i < $string_num; $i++)
@@ -14,5 +18,5 @@ function wiki($haku)
 			$result = "http://en.wikipedia.org/wiki/".ucfirst($result); #Eka kirjain isoksi
 			return $result;
 		}
-
+		}
 ?>
