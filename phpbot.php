d3b98ab8f2267bae98928f3d3f32d66f93da9de4
Jaakko Kuusela <shimatta@gmail.com>
2013-07-10 14:55:21 +0300

    Fixed the mess :D

--- phpbot.php
+++ phpbot.php
@@ -76,14 +76,11 @@ function loop($config)
 		} else {
 			while ($nick == "433")
 			{
-<<<<<<< HEAD
 				$nick = $this->send_data("NICK ".$config["config"]["nick"].rand(1,10));
 				sleep(1);
 			}
 			foreach (explode(",", $config["config"]["chans"]) as $chan)
 			{		
-=======
->>>>>>> 778eda7f62aeca037b4c57bf6ffdb0cc63e21e39
 				$this->join_channel($chan);
 			}
 		}
