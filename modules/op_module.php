15343af3cf3154b24d23dd353ff87993d5516efa
Marko Mäkilä <hrna@lakka.kapsi.fi>
2013-07-11 14:44:26 +0300

    Checking if parameter is given, otherwise give how-to-do

--- modules/op_module.php
+++ modules/op_module.php
@@ -4,8 +4,14 @@ function op ($data, $config)
 	{
 		if (oper_auth($data,$config))
 		{
-			$mode = "MODE ".$data[2]." +o ".$data[4]."\r\n";
-			return $mode;
+			if (isset($data[4]))
+			{
+				$mode = "MODE ".$data[2]." +o ".$data[4]."\r\n";
+				return $mode;
+			} else {
+				$msg = "Usage: !op <nick>";
+				return $msg;	
+			}
 		} else {
 			return get_nick($data).", Ei taida sun natsat riittää :(\r\n"; 
 		}
