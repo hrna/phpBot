6c6174cc2cf000ad0c5f889cb3c3f3c3ccf97117
Jaakko Kuusela <shimatta@gmail.com>
2013-07-11 12:04:39 +0300

    Made FMI better

--- modules/fmi_module.php
+++ modules/fmi_module.php
@@ -1,16 +1,22 @@
 <?
 
-function fmi($data, $config) 
+function fmi($data) 
 {
-	$saa = "";
+	$saa = ucfirst($data[4]);
 	$dom = new DOMDocument();
 	@$dom->loadHTMLFile("http://ilmatieteenlaitos.fi/saa/".$data[4]);
 
 	$xpath = new DOMXPath($dom);
 
+		foreach ($xpath->query('//span[@class="time-stamp"]') as $tag) {
+    		//print_r($tag);
+    		$saa .= " klo ".substr($tag->nodeValue,10,5).": ";
+		}
+
 		foreach ($xpath->query('//span[contains(@class,"parameter-name-value")]') as $tag) {
     		//print_r($tag);
     		$saa .= trim($tag->nodeValue)." - ";
 		}
+		$saa = str_replace(array("\n", "\r"), '', $saa);
 		return $saa;
 }
