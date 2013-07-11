e16485daeae2e63f9503d8fd7b1c59701d46a63f
Jaakko Kuusela <shimatta@gmail.com>
2013-07-11 20:55:53 +0300

    blah

--- modules/title_module.php
+++ modules/title_module.php
@@ -21,18 +21,19 @@ if(substr($url, 0, 7) != "http://" && substr($url, 0, 8) != "https://")
 {
 	$url = "http://".$url;
 }
-
+#echo filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_HOST_REQUIRED);
 #tarkastetaan URL
 if (filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_HOST_REQUIRED) !== false)
 {
 	ini_set("user_agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0");
 	$dom = new DOMDocument();
-	echo $url;
+	#echo $url;
+	$dom->strictErrorChecking = FALSE;
 	@$dom->loadHTMLFile($url);
-
+	#echo $dom->saveHTML();
 	// find the title
 	$titlelist = $dom->getElementsByTagName("title");
-
+	print_r($titlelist);
 	if($titlelist->length > 0){
   		return "Title: ".$titlelist->item(0)->nodeValue;
  	}
