7274e6779c04a5b951745b85b87cbad5ffeae338
Marko Mäkilä <hrna@lakka.kapsi.fi>
2013-07-11 11:01:59 +0300

    fixed

--- modules/op_module.php
+++ modules/op_module.php
@@ -1,13 +1,14 @@
 <?php
-//class op extends tsunku {
+
 function op ($data, $config)
 	{
-		//if (in_array(get_nick($data), $config["opers"]))
-		//{
-			print_r($data);
-			$pylly = "MODE ".$data[2]." +o ".$data[4]."\r\n";
-			return $pylly;
-		//} else { return get_nick($data).", Ei taida sun natsat riittää :(\r\n"; }
+		if (in_array(get_nick($data), $config["opers"]))
+		{
+			$mode = "MODE ".$data[2]." +o ".$data[4]."\r\n";
+			return $mode;
+		} else {
+			return get_nick($data).", Ei taida sun natsat riittää :(\r\n"; 
+		}
 	}			
-//}
+
 ?>
