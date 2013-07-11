d8d4abd52fa98853a303bf77378e50064ccc7f9c
Jaakko Kuusela <shimatta@gmail.com>
2013-07-11 22:15:51 +0300

    It's alive

--- modules/title_module.php
+++ modules/title_module.php
@@ -3,6 +3,8 @@
 function title($data)
 {
 
+require_once("lib/simple_html_dom.php");
+
 #$reg_exp = "@^(http\:\/\/|https\:\/\/)?([a-z0-9][a-z0-9\-]*\.)+[a-z0-9][a-z0-9\-]*$@i";
 
 #tuleeko kanavalta, ei komentona
@@ -21,29 +23,31 @@ if(substr($url, 0, 7) != "http://" && substr($url, 0, 8) != "https://")
 {
 	$url = "http://".$url;
 }
-#echo filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_HOST_REQUIRED);
+
 #tarkastetaan URL
 if (filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_HOST_REQUIRED) !== false)
 {
-	ini_set("user_agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0");
-	$dom = new DOMDocument();
-	#echo $url;
-	$dom->strictErrorChecking = FALSE;
-	@$dom->loadHTMLFile($url);
-	#echo $dom->saveHTML();
-	// find the title
-	$titlelist = $dom->getElementsByTagName("title");
-	print_r($titlelist);
-	if($titlelist->length > 0){
-  		return "Title: ".$titlelist->item(0)->nodeValue;
- 	}
+	#ini_set("user_agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0");
 
+ 	$curl = curl_init(); 
+ 	curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0");
+	curl_setopt($curl, CURLOPT_URL, $url);  
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);  
+	curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10);  
+	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE);
+	$str = curl_exec($curl);  
+	curl_close($curl); 
+ 	$html = str_get_html($str);
+ 	
+ 	foreach($html->find('title') as $element)
+ 	{
+       return "Title: ".trim($element->plaintext);
+	}
 } 
 
 //} else {
 //	return "Invalid URL";
 //}
 }
-
 
 ?>
