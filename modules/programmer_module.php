d4a88718b9c7aa65529630fef31e41b3b5466879
Jaakko Kuusela <shimatta@gmail.com>
2013-07-11 11:20:48 +0300

    made the first letter lower case

--- modules/programmer_module.php
+++ modules/programmer_module.php
@@ -8,7 +8,7 @@ function programmer ($data, $config)
 	$line_amount = count($lines);
 	$random = rand(0, $line_amount-1);
 	
-	return "You might be a programmer if ".$lines[$random];
+	return "You might be a programmer if ".lcfirst($lines[$random]);
 
 }
 
