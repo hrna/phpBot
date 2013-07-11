4138462ea328d33739376b8f7791236b06702946
Jaakko Kuusela <shimatta@gmail.com>
2013-07-11 23:39:40 +0300

    Cleaned up title module

--- modules/title_system_module.php
+++ modules/title_system_module.php
@@ -5,24 +5,7 @@ function sys_title($data)
 
 require_once("lib/simple_html_dom.php");
 
-#$reg_exp = "@^(http\:\/\/|https\:\/\/)?([a-z0-9][a-z0-9\-]*\.)+[a-z0-9][a-z0-9\-]*$@i";
-
-#tuleeko kanavalta, ei komentona
-#if (trim($data[3]) != ":!title") {
-#	$url = trim(substr($data[3], 1));
-#} else {	
-#	$url = trim($data[4]);
-#}
 $url = $data;
-#onko URL oikean muotoinen
-//if(preg_match($reg_exp, $url) == TRUE)
-//{
-
-#lisätään http:// jos ei ole
-if(substr($url, 0, 7) != "http://" && substr($url, 0, 8) != "https://")
-{
-	$url = "http://".$url;
-}
 
 #tarkastetaan URL
 if (filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_HOST_REQUIRED) !== false)
@@ -44,10 +27,6 @@ if (filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_HOST_REQUIRED) !== false)
        return "Title: ".trim($element->plaintext);
 	}
 } 
-
-//} else {
-//	return "Invalid URL";
-//}
 }
 
 ?>
