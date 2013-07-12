f60acab596702e74072474096015eb77502b6f1c
Marko Mäkilä <hrna@lakka.kapsi.fi>
2013-07-12 21:22:36 +0300

    Poistettu turhaa tauhkaa

--- modules/cmd_module.php
+++ modules/cmd_module.php
@@ -5,10 +5,7 @@ function cmd($data, $config)
 	$cmd = "";
 	foreach ($config["modules"] as $mod)
 	{
-		if ($mod != "get_nick")
-		{
 		$cmd .= "!".$mod." ";
-		}
 	}
 	return get_nick($data).": käytössä olevat komennot ovat: ".$cmd." ";
 
